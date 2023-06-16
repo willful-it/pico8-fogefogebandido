0a46629e5e574f703a5835dbddf8c56af863ff2d
Renato Torres <renato@torres.id>
2023-06-16 23:48:13 +0100

    feat: working new physics

--- constants.lua
+++ constants.lua
@@ -1,23 +1,5 @@
-  --physics
-  const_max_velocity=new_vector2(80,100)
-  const_max_velocity_x_offground=100
-  const_acceleration_x_offground=100
-  const_acceleration=new_vector2(0.5,0.5)
-  const_time_delta=1/50
-  const_gravity=250
-  const_max_jumps=1
-  const_max_jump_height=20 --pixels
-  const_animation_fps=5
-
-  --keys
-  k_left=0
-  k_right=1
-  k_up=2
-  k_down=3
-  k_jump=4
-  k_shot=5
-
-  --state
-  stand=1
-  walk=2
-  jump=3
+accel=0.25
+fric=0.8
+gravity_jump=0.15
+gravity=0.25
+limit_jump_timer=4
--- main.lua
+++ main.lua
@@ -1,94 +1,53 @@
-function key_state(chr,k)
-  --chr is a character
-  --k is the key code
-  local key_index=k+1
-  return chr.inputs[key_index]
+function _init()
+  load_room(0,0)
+
+  player0={
+    tile=1,
+    number=0,
+    draw=draw_player,
+    update=update_player,
+    init=init_player,
+    spr_move={1,2,3},
+    spr_jump={3},
+    spr_stand={1}
+  }
+  init_object(player0,0,0)
+
+  player1={
+    tile=1,
+    number=1,
+    draw=draw_player,
+    update=update_player,
+    init=init_player,
+    spr_move={22,23,24},
+    spr_jump={24},
+    spr_stand={22}
+  }
+  init_object(player1,100,0)
+
+  player2={
+    tile=1,
+    number=2,
+    draw=draw_player,
+    update=update_player,
+    init=init_player,
+    spr_move={33,34,35},
+    spr_jump={35},
+    spr_stand={33}
+  }
+  init_object(player2,50,0)
 end
 
-function _init()
-  --players
-  players={}
-
-  --player1
-  local sprites0={
-    {6},
-    {6,7,8},
-    {8}
-  }
-  local weapon0={
-    base=27,
-    shooting={28}
-  }
-  local start_position0=new_vector2(10,0)
-  player0=new_character(0,sprites0,weapon0,start_position0)
-  player0.debug=true
-  add(players,player0)
-
-  --player2
-  local sprites1={
-    {22},
-    {22,23,24},
-    {24}
-  }
-  local weapon1={
-    base=11,
-    shooting={12}
-  }
-  local start_position1=new_vector2(105,0)
-  player1=new_character(1,sprites1,weapon1,start_position1)
-  player1.flip=true
-  add(players,player1)
+function _update()
+  foreach(objects,update_object)
 end
 
 function _draw()
-  cls()
-  for p in all(players) do
-    if p.debug then
-      print_physics(p)
-    end
-    animate_character(p)
-    animate_character_weapon(p)
-  end
-end
+  -- clear the screen
+  cls(12)
 
-function _update60()
-  for p in all(players) do
-    p.inputs={
-      btn(k_left, p.number),
-      btn(k_right, p.number),
-      btn(k_up, p.number),
-      btn(k_down, p.number),
-      btnp(k_jump, p.number),
-      btn(k_shot, p.number)
-    }
+  map(room.x,room.y,0,0)
 
-    -- if collide_map(p.position.x+1,p.position.y,7,7) then
-    --   p.against_right_wall=true
-    -- else
-    --   p.against_right_wall=false
-    -- end
-
-    -- if collide_map(p.position.x-1,p.position.y,7,7) then
-    --   p.against_left_wall=true
-    -- else
-    --   p.against_left_wall=false
-    -- end
-
-    -- if collide_map(p.position.x,p.position.y+1,7,7) then
-    --   p.on_ground=true
-    -- else
-    --   p.on_ground=false
-    -- end
-
-    update_character(p)
-  end
-end
-
-function round(f)
-  local diff=(f-flr(f))*10
-  if diff>5 then
-    return ceil(f)
-  else
-    return flr(f)
-  end
+  -- draw objects
+  foreach(objects,draw_object)
 end
